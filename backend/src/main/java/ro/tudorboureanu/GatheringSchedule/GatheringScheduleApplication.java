1ed0073f2049abd67400f67572384a98b51dc030
Tudor Boureanu <tudor04boureanu@gmail.com>
2026-08-24 23:55:56 +0300

    backend initialization: Initialized project with basic information in application.yml

---
+++ backend/src/main/java/ro/tudorboureanu/GatheringSchedule/GatheringScheduleApplication.java
@@ -0,0 +1,13 @@
+package ro.tudorboureanu.GatheringSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatheringScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatheringScheduleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ro/tudorboureanu/GatheringSchedule/GatheringScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.tudorboureanu.GatheringSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatheringScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
